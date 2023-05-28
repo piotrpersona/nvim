e6c3ae773c07cd583ce23ff8a930d8379be064fb
Piotr Persona <persona.piotr@gmail.com>
2023-05-28 21:31:00 +0200

    feat: tabnine 1,2s
    Signed-off-by: Piotr Persona <persona.piotr@gmail.com>

--- after/plugin/tabnine.lua
+++ after/plugin/tabnine.lua
@@ -2,7 +2,7 @@ require('tabnine').setup({
   disable_auto_comment=true,
   accept_keymap="<Tab>",
   dismiss_keymap = "<C-]>",
-  debounce_ms = 500,
+  debounce_ms = 1200,
   suggestion_color = {gui = "#808080", cterm = 244},
   exclude_filetypes = {"TelescopePrompt"},
   log_file_path = nil, -- absolute path to Tabnine log file

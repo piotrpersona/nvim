938e9bfe63c285871a04a46831e79dc1afb12467
Piotr Persona <persona.piotr@gmail.com>
2023-05-28 21:26:51 +0200

    feat: goimports
    Signed-off-by: Piotr Persona <persona.piotr@gmail.com>

--- lua/pp/autocmd.lua
+++ lua/pp/autocmd.lua
@@ -2,6 +2,6 @@ vim.api.nvim_create_autocmd(
     "BufWritePost",
     {
         pattern = "*.go",
-        command = "silent !gofumpt -w <afile>:p:S",
+        command = "silent !goimports -local -w <afile>:p:S && gofumpt -w <afile>:p:S",
     }
 )

cc0c3aa7640a1511a01565ad6bc88d0a44b60683
Piotr Persona <persona.piotr@gmail.com>
2023-10-19 22:12:23 +0200

    add black python
    Signed-off-by: Piotr Persona <persona.piotr@gmail.com>

--- lua/pp/autocmd.lua
+++ lua/pp/autocmd.lua
@@ -1,7 +1,8 @@
-vim.api.nvim_create_autocmd(
-    "BufWritePost",
-    {
-        pattern = "*.go",
-        command = "silent !goimports -local -w <afile>:p:S && gofumpt -w <afile>:p:S",
-    }
-)
+vim.api.nvim_create_autocmd("BufWritePost", {
+    pattern = "*.go",
+    command = "silent !goimports -local -w <afile>:p:S && gofumpt -w <afile>:p:S",
+})
+vim.api.nvim_create_autocmd("BufWritePost", {
+    pattern = "*.py",
+    command = "silent !black <afile>:p:S",
+})

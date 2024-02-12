5e57bbbdf9f39f1cc2692fc1b5eef1f0c560359e
Piotr Persona <persona.piotr@gmail.com>
2024-02-12 20:23:03 +0100

    Set popup highlight
    Signed-off-by: Piotr Persona <persona.piotr@gmail.com>

--- lua/pp/set.lua
+++ lua/pp/set.lua
@@ -40,3 +40,5 @@ vim.opt.cursorline = true
 -- Cursor
 vim.api.nvim_set_hl(0, 'Cursor', { bg = '#b55387' } )
 vim.api.nvim_set_hl(0, 'iCursor', { fg = '#ffffff', bg = '#4fb2bd' } )
+-- Popup menu
+vim.api.nvim_set_hl(0, 'PmenuSel', { bg = '#8153b5' } )

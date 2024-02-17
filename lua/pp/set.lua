c57c97c0f0deeaef489e80d0c5673faed0af451f
Piotr Persona <persona.piotr@gmail.com>
2024-02-17 14:08:53 +0100

    theme: tokynonight-moon
    Signed-off-by: Piotr Persona <persona.piotr@gmail.com>

--- lua/pp/set.lua
+++ lua/pp/set.lua
@@ -33,7 +33,7 @@ vim.opt.colorcolumn = "80"
 vim.opt.splitbelow = true
 vim.opt.splitright = true
 
-vim.cmd [[colorscheme melange]]
+vim.cmd [[colorscheme tokyonight-moon]]
 vim.opt.guicursor = 'n-v-c:block-Cursor,i:block-iCursor-blinkwait300-blinkon200-blinkoff150'
 vim.api.nvim_set_hl(0, 'LineNr', { fg = '#b55387' })
 vim.api.nvim_set_hl(0, 'CursorLine', { bg = '#42363c' })

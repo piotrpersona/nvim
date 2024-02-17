59cf3a7e31ac78aac3c4e4f31624598b512a5dc7
Piotr Persona <persona.piotr@gmail.com>
2024-02-17 14:16:31 +0100

    add theme.lua
    Signed-off-by: Piotr Persona <persona.piotr@gmail.com>

--- lua/pp/set.lua
+++ lua/pp/set.lua
@@ -1,3 +1,5 @@
+local theme = require("pp.theme")
+
 vim.opt.nu = true
 vim.opt.relativenumber = true
 
@@ -34,13 +36,4 @@ vim.opt.splitbelow = true
 vim.opt.splitright = true
 
 vim.cmd [[colorscheme tokyonight-moon]]
-vim.opt.guicursor = 'n-v-c:block-Cursor,i:block-iCursor-blinkwait300-blinkon200-blinkoff150'
-vim.api.nvim_set_hl(0, 'LineNr', { fg = '#b55387' })
-vim.api.nvim_set_hl(0, 'CursorLine', { bg = '#42363c' })
-vim.opt.cursorline = true
--- Cursor
-vim.api.nvim_set_hl(0, 'Cursor', { bg = '#b55387' })
-vim.api.nvim_set_hl(0, 'iCursor', { fg = '#ffffff', bg = '#4fb2bd' })
--- Popup menu
-vim.api.nvim_set_hl(0, 'PmenuSel', { bg = '#b08e64' })
-
+setColorScheme()

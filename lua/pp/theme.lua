5bb925fc9681a49de8b89958b824cf799db11a8c
Piotr Persona <persona.piotr@gmail.com>
2024-02-17 14:24:27 +0100

    highligh currentlinenr
    Signed-off-by: Piotr Persona <persona.piotr@gmail.com>

--- lua/pp/theme.lua
+++ lua/pp/theme.lua
@@ -2,6 +2,7 @@ function setColorScheme()
     vim.opt.guicursor = 'n-v-c:block-Cursor,i:block-iCursor-blinkwait300-blinkon200-blinkoff150'
     vim.api.nvim_set_hl(0, 'LineNr', { fg = '#b55387' })
     vim.api.nvim_set_hl(0, 'CursorLine', { bg = '#42363c' })
+    vim.api.nvim_set_hl(0, 'CursorLineNr', { fg = '#d9bf41' })
     vim.opt.cursorline = true
     -- Cursor
     vim.api.nvim_set_hl(0, 'Cursor', { bg = '#b55387' })

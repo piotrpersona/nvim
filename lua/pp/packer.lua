0eef8968b96a19b2c4a2cb58d647c66615e6a050
Piotr Persona <persona.piotr@gmail.com>
2024-02-18 08:06:53 +0100

    vimspector
    Signed-off-by: Piotr Persona <persona.piotr@gmail.com>

--- lua/pp/packer.lua
+++ lua/pp/packer.lua
@@ -7,68 +7,68 @@ return require('packer').startup(function(use)
   -- Packer can manage itself
   use 'wbthomason/packer.nvim'
   use {
-	  'nvim-telescope/telescope.nvim', tag = '0.1.4',
-	  requires = { {'nvim-lua/plenary.nvim'} }
+    'nvim-telescope/telescope.nvim', tag = '0.1.4',
+    requires = { { 'nvim-lua/plenary.nvim' } }
   }
   use 'folke/tokyonight.nvim'
 
-  use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
+  use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
   use('nvim-treesitter/playground')
   use('theprimeagen/harpoon')
   use('mbbill/undotree')
   use('tpope/vim-fugitive')
   use {
-	  'VonHeikemen/lsp-zero.nvim',
-	  branch = 'v2.x',
-	  requires = {
-		  -- LSP Support
-		  {'neovim/nvim-lspconfig'},             -- Required
-		  {                                      -- Optional
-		  'williamboman/mason.nvim',
-		  run = function()
-			  pcall(vim.cmd, 'MasonUpdate')
-		  end,
-	  },
-	  {'williamboman/mason-lspconfig.nvim'}, -- Optional
+    'VonHeikemen/lsp-zero.nvim',
+    branch = 'v2.x',
+    requires = {
+      -- LSP Support
+      { 'neovim/nvim-lspconfig' }, -- Required
+      {                        -- Optional
+        'williamboman/mason.nvim',
+        run = function()
+          pcall(vim.cmd, 'MasonUpdate')
+        end,
+      },
+      { 'williamboman/mason-lspconfig.nvim' }, -- Optional
 
-	  -- Autocompletion
-	  {'hrsh7th/nvim-cmp'},     -- Required
-	  {'hrsh7th/cmp-nvim-lsp'}, -- Required
-	  {'L3MON4D3/LuaSnip'},     -- Required
+      -- Autocompletion
+      { 'hrsh7th/nvim-cmp' }, -- Required
+      { 'hrsh7th/cmp-nvim-lsp' }, -- Required
+      { 'L3MON4D3/LuaSnip' }, -- Required
+    }
   }
-}
-use 'nvim-tree/nvim-web-devicons'
-use { 'sindrets/diffview.nvim', requires = 'nvim-lua/plenary.nvim' }
-use('ryanoasis/vim-devicons')
-use('HiPhish/nvim-ts-rainbow2')
-use('fatih/vim-go')
-use {'nvim-telescope/telescope-fzf-native.nvim', run = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build' }
-use({ 'rose-pine/neovim', as = 'rose-pine' })
-use { "catppuccin/nvim", as = "catppuccin" }
-use('preservim/nerdcommenter')
-use {
-  'nvim-lualine/lualine.nvim',
-  requires = { 'nvim-tree/nvim-web-devicons', opt = true }
-}
-use {'romgrk/barbar.nvim', requires = {
-  'lewis6991/gitsigns.nvim', -- OPTIONAL: for git status
-  'nvim-tree/nvim-web-devicons', -- OPTIONAL: for file icons
-}}
--- using packer.nvim
-use {
-  'nmac427/guess-indent.nvim',
-  config = function() require('guess-indent').setup {} end,
-}
-use {
-  'nvim-lualine/lualine.nvim',
-  requires = { 'nvim-tree/nvim-web-devicons', opt = true }
-}
-use "EdenEast/nightfox.nvim"
-use 'Mofiqul/dracula.nvim'
-use "savq/melange-nvim"
-use 'YacineDo/mc.nvim'
-use 'piotrpersona/tele-conda'
-use "lukas-reineke/indent-blankline.nvim"
-use 'prichrd/netrw.nvim'
+  use 'nvim-tree/nvim-web-devicons'
+  use { 'sindrets/diffview.nvim', requires = 'nvim-lua/plenary.nvim' }
+  use('ryanoasis/vim-devicons')
+  use('HiPhish/nvim-ts-rainbow2')
+  use('fatih/vim-go')
+  use { 'nvim-telescope/telescope-fzf-native.nvim', run = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build' }
+  use({ 'rose-pine/neovim', as = 'rose-pine' })
+  use { "catppuccin/nvim", as = "catppuccin" }
+  use('preservim/nerdcommenter')
+  use {
+    'nvim-lualine/lualine.nvim',
+    requires = { 'nvim-tree/nvim-web-devicons', opt = true }
+  }
+  use { 'romgrk/barbar.nvim', requires = {
+    'lewis6991/gitsigns.nvim',   -- OPTIONAL: for git status
+    'nvim-tree/nvim-web-devicons', -- OPTIONAL: for file icons
+  } }
+  -- using packer.nvim
+  use {
+    'nmac427/guess-indent.nvim',
+    config = function() require('guess-indent').setup {} end,
+  }
+  use {
+    'nvim-lualine/lualine.nvim',
+    requires = { 'nvim-tree/nvim-web-devicons', opt = true }
+  }
+  use "EdenEast/nightfox.nvim"
+  use 'Mofiqul/dracula.nvim'
+  use "savq/melange-nvim"
+  use 'YacineDo/mc.nvim'
+  use 'piotrpersona/tele-conda'
+  use "lukas-reineke/indent-blankline.nvim"
+  use 'prichrd/netrw.nvim'
+  use 'puremourning/vimspector'
 end)
-

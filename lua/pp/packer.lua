22a1294a29d973c9eaa448e39bd0ffde17f61ae1
Piotr Persona <persona.piotr@gmail.com>
2024-06-15 19:08:24 +0200

    feat: add oil
    Signed-off-by: Piotr Persona <persona.piotr@gmail.com>

--- lua/pp/packer.lua
+++ lua/pp/packer.lua
@@ -69,5 +69,10 @@ return require('packer').startup(function(use)
   use 'ayu-theme/ayu-vim'
   use 'techtuner/aura-neovim'
   use({ 'rose-pine/neovim', as = 'rose-pine' })
-
+  use({
+    "stevearc/oil.nvim",
+    config = function()
+      require("oil").setup()
+    end,
+  })
 end)

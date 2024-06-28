7a1c1529da037e8ad1647e5e0ebbaa52a561c124
Piotr Persona <persona.piotr@gmail.com>
2024-06-29 00:29:55 +0200

    fix: Alias
    Signed-off-by: Piotr Persona <persona.piotr@gmail.com>

--- lua/pp/remap.lua
+++ lua/pp/remap.lua
@@ -45,7 +45,7 @@ vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 
-vim.keymap.set("n", "<leader>vpp", "<cmd>e ~//.config/nvim/lua/pp/packer.lua<CR>");
+vim.keymap.set("n", "<leader>vpp", "<cmd>e ~/.config/nvim/lua/pp/packer.lua<CR>");
 vim.keymap.set("n", "<leader>mr", "<cmd>CellularAutomaton make_it_rain<CR>");
 
 -- pane split

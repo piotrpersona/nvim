94260e05cfd7422dcd0f7074581e580d6a5685ba
Piotr Persona <persona.piotr@gmail.com>
2024-06-30 18:35:25 +0200

    fix: autocmd
    Signed-off-by: Piotr Persona <persona.piotr@gmail.com>

--- lua/pp/autocmd.lua
+++ lua/pp/autocmd.lua
@@ -1,10 +1,10 @@
--- vim.api.nvim_create_autocmd("BufWritePost", {
---     pattern = "*.go",
---     command = "silent !goimports -local -w <afile>:p:S && gofumpt -w <afile>:p:S",
--- })
--- vim.api.nvim_create_autocmd("BufWritePost", {
---     pattern = "*.py",
---     command = "silent !black <afile>:p:S",
--- })
+vim.api.nvim_create_autocmd("BufWritePost", {
+    pattern = "*.go",
+    command = "silent !goimports -local -w <afile>:p:S && gofumpt -w <afile>:p:S",
+})
+vim.api.nvim_create_autocmd("BufWritePost", {
+    pattern = "*.py",
+    command = "silent !black <afile>:p:S",
+})
 
 vim.g.netrw_bufsettings = 'noma nomod nu rnu nobl nowrap ro'

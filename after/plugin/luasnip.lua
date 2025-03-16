c9e0534714375508bc2f562dde8f09a65d972a2c
Piotr Persona <persona.piotr@gmail.com>
2025-03-16 13:16:11 +0100

    feat: Add ife and ifr snippets
    Signed-off-by: Piotr Persona <persona.piotr@gmail.com>

--- after/plugin/luasnip.lua
+++ after/plugin/luasnip.lua
@@ -4,14 +4,34 @@ local text = ls.text_node
 local insert = ls.insert_node
 
 ls.add_snippets("go", {
+    -- if err != nil {
+    --     return err
+    -- }
+    snippet("ife", {
+        text({ "if err != nil {", "\treturn err", "}" }),
+    }),
+    -- if err != nil {
+    --     return <i>
+    -- }
+    snippet("ifr", {
+        text({ "if err != nil {", "\treturn " }),
+        insert(1, ''),
+        text({ "", "}" }),
+    }),
+    -- if err != nil {
+    --     return errors.Wrapf(err, "<i>")
+    -- }
     snippet("iwf", {
         text({ "if err != nil {", "\treturn errors.Wrapf(err, \"" }),
-        insert(1, ''), -- Cursor inside the function
+        insert(1, ''),
         text({ "\")", "}" }),
     }),
+    -- if err != nil {
+    --     return errors.Wrap(err, "<i>")
+    -- }
     snippet("iwe", {
         text({ "if err != nil {", "\treturn errors.Wrap(err, \"" }),
-        insert(1, ''), -- Cursor inside the function
+        insert(1, ''),
         text({ "\")", "}" }),
     }),
 })

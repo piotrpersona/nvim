c853a3ac15f736c0511aeebf7ceb717b243496fb
Piotr Persona <persona.piotr@gmail.com>
2025-03-19 12:36:59 +0100

    feat: explicit formatting
    Signed-off-by: Piotr Persona <persona.piotr@gmail.com>

--- lua/pp/autocmd.lua
+++ lua/pp/autocmd.lua
@@ -1,16 +1,10 @@
--- vim.api.nvim_create_autocmd("BufWritePost", {
---     pattern = "*.go",
---     command = "silent !goimports -local -w <afile>:p:S && gofumpt -w <afile>:p:S",
--- })
--- vim.api.nvim_create_autocmd("BufWritePost", {
---     pattern = "*.py",
---     command = "silent !black <afile>:p:S",
--- })
-vim.api.nvim_create_autocmd("BufWritePre", {
-  pattern = "*.go",
-  callback = function()
-    vim.lsp.buf.formatting_sync(nil, 1000)
-  end,
+vim.api.nvim_create_autocmd("BufWritePost", {
+    pattern = "*.go",
+    command = "silent !goimports -local -w <afile>:p:S && gofumpt -w <afile>:p:S",
+})
+vim.api.nvim_create_autocmd("BufWritePost", {
+    pattern = "*.py",
+    command = "silent !black <afile>:p:S",
 })
 
 vim.g.netrw_bufsettings = 'noma nomod nu rnu nobl nowrap ro'
